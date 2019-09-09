688b6be7ae431068b4a9fb74afdbb7e61c4121c6
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-09 13:50:35 -0400

    Laws Controller page, only for index and show, no need for anything else

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -1,2 +1,29 @@
 class LawsController < ApplicationController
+    
+    before_action :set_law, only: [:show]
+
+  # GET /laws
+  def index
+   @laws = Law.all
+   render json: @laws
+  end
+
+  # GET /laws/1
+  def show
+   render json: @law
+  end
+  
+  
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_law
+   @law = Law.find(params[:id])
+  end
+  # Only allow a trusted parameter “white list” through.
+  def law_params
+  params.require(:law).permit(:name, :description)
+  end
+  
+end
+
 end
